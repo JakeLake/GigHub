8f85a0e769388c841a2bcc3e49a681a15e3d3a7f
Jakob <Jakob>
2017-09-28 08:47:42 +0200

    Updating to 4.6.1 adding authentification

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -10,6 +10,7 @@ namespace GigHub.Controllers {
         public GigsController() {
             Context = new ApplicationDbContext();
         }
+        [Authorize]
         public ActionResult Create() {
             var viewModel = new GigFormViewModel {
                 Genres = Context.Genres.ToList()
